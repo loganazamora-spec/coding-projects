6d336661bf07a444df5f88a980a5db4b441b36a3
Logan Zamora <loganazamora@gmail.com>
2026-09-25 17:48:38 +0300

    Created first.java. Needed to figure out Lulea issue in add.py

--- java/first.java
+++ java/first.java
@@ -1,1 +1,35 @@
+void main() {
+
+    IO.println("Hello, world!");
+
+    int age;
+
+    age = 18;
+
+    String age_declaration = "I am " + age + " years old.";
+
+    IO.println(age_declaration);
+    
+    secondLine();
+    data_cast();
+}
+
+
+
+void secondLine() {
+    IO.print("This is the second line. ");
+
+    IO.println("As in: " + 2);
+}
+
+void data_cast() {
+    double avg_class_age = (16+18+18+16+19+18+17+16+18)/9.0;
+
+    IO.println("Average class age: " + String.format("%.2f", avg_class_age));
+
+    IO.print("But, more cleanly: ");
+
+    IO.println((int) avg_class_age);
+
+} 
 
---
+++ python/CS50/add.py
@@ -0,0 +1,10 @@
+import random
+
+def add_numbers(a,b):
+    return a+b
+
+a = random.randint(1,10)
+b = random.randint(20,30)
+
+print(add_numbers(a,b))
+
